e5014502e2df9f3176db0bfcb5b141c73a5b3220
ygor-sena <yde-goes@student.42sp.org.br>
2024-08-15 18:40:44 -0300

    refactor: function usage only in ex04

--- D00/ex00/Hello.py
+++ D00/ex00/Hello.py
@@ -1,30 +1,26 @@
-def first_python_script():
-	ft_list = ["Hello", "tata!"]
-	ft_tuple = ("Hello", "toto!")
-	ft_set = {"Hello", "tutu!"}
-	ft_dict = {"Hello" : "titi!"}
+ft_list = ["Hello", "tata!"]
+ft_tuple = ("Hello", "toto!")
+ft_set = {"Hello", "tutu!"}
+ft_dict = {"Hello": "titi!"}
 
-	# Change list value
-	ft_list[1] = "World!"
+# Change list value
+ft_list[1] = "World!"
 
-	# Change tuple value
-	tmp = list(ft_tuple)
-	tmp[1] = "Brazil!"
-	ft_tuple = tuple(tmp)
+# Change tuple value
+tmp = list(ft_tuple)
+tmp[1] = "Brazil!"
+ft_tuple = tuple(tmp)
 
-	# Change set value
-	ft_set.remove("tutu!")
-	ft_set.update(["São Paulo!"])
-	# Or use union to concatenate two sets:
-	# ft_set |= {"São Paulo!"}
+# Change set value
+ft_set.remove("tutu!")
+ft_set.update(["São Paulo!"])
+# Or use union to concatenate two sets:
+# ft_set |= {"São Paulo!"}
 
-	# Change dict value
-	ft_dict["Hello"] = "42SãoPaulo!"
+# Change dict value
+ft_dict["Hello"] = "42SãoPaulo!"
 
-	print(ft_list)
-	print(ft_tuple)
-	print(ft_set)
-	print(ft_dict)
-
-if __name__ == '__main__':
-	first_python_script()
+print(ft_list)
+print(ft_tuple)
+print(ft_set)
+print(ft_dict)
