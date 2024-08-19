bc28757529dfbecbb046423b16aa270864b87186
ygor-sena <yde-goes@student.42sp.org.br>
2024-08-19 09:41:30 -0300

    feat: add finished ex02 of D00

---
+++ D00/ex02/find_ft_type.py
@@ -0,0 +1,15 @@
+def all_thing_is_obj(object: any) -> int:
+    if isinstance(object, str):
+        print(f'{object} is in the kitchen : {type(object)}')
+    elif isinstance(object, list):
+        print(f'List : {type(object)}')
+    elif isinstance(object, tuple):
+        print(f'Tuple : {type(object)}')
+    elif isinstance(object, set):
+        print(f'Set : {type(object)}')
+    elif isinstance(object, dict):
+        print(f'Dict : {type(object)}')
+    else:
+        print('Type not found')
+
+    return 42
